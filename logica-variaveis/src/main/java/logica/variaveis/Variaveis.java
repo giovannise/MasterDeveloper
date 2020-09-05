9b07ecb8e2b2d49fb4a62f5cc2ee87d6de265616
Giovannise <giovannise_lisboa@hotmail.com>
2020-09-05 20:29:51 -0300

    Exercício sobre variáveis

---
+++ logica-variaveis/src/main/java/logica/variaveis/Variaveis.java
@@ -0,0 +1,34 @@
+package logica.variaveis;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+
+		//int idade;
+		
+		//idade = 20;
+		
+		int idade = 20;
+		double nota = 5.9;
+		boolean ativo = true; //ou false
+		String nome = "Gi";
+		
+		System.out.println(nota);
+		
+		System.out.println(nome + " -> " + nota);
+		
+		//int 2x; não pode
+		int x2;
+		
+		//nome de variaveis = minúsculas
+
+		//nomes contatenados:
+		String numeroDePessoas; //camelCase
+		
+		var x = 5;
+		//variável genérica, vai definir de acordo com o conteúdo dela
+		//não indicado.
+
+	}
+
+}
